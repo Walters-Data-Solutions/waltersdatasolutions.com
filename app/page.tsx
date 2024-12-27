15fb20a233df7cf5b440efb7f77f41842cbb0d14
Ty Walters <Ty.Walters97@gmail.com>
2024-12-27 16:02:01 -0700

    fix the link

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import Link from 'next/link'
 import Home from './components/Home'
 import About from './components/About'
 import Services from './components/Services'
